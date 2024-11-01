115020561613d718f85c8f8e2e794e4fb402f3e8
Chris0028 <gec0028228@est.univalle.edu>
2024-11-01 08:46:03 -0400

    FilePreview

--- mosquito-laboratorio-app/src/modules/pdf/services/pdfService.js
+++ mosquito-laboratorio-app/src/modules/pdf/services/pdfService.js
@@ -1,3 +1,14 @@
+import { httpClient } from "../../../api/httpClient/httpClient";
+
 export async function getFileWithResultAsync(fileId) {
-    console.log(fileId);
+    const res = await httpClient.post('/File/GetFileWithResult', fileId);
+    if (res.status === 200) {
+        return deserealizeObject(res.data.ufc_get_file_with_result);
+    } else {
+        console.log('ERROR');
+    }
+}
+
+function deserealizeObject(object) {
+    return JSON.parse(object);
 }
