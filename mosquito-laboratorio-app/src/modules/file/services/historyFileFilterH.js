b90d7c218c710f96328aea5de73d9fe85658f399
teddyFernandez7 <FLT1010288@est.univalle.edu>
2024-11-22 00:39:19 -0400

    Finish

--- mosquito-laboratorio-app/src/modules/file/services/historyFileFilterH.js
+++ mosquito-laboratorio-app/src/modules/file/services/historyFileFilterH.js
@@ -3,7 +3,6 @@ import { httpClient } from "../../../api/httpClient/httpClient";
 export async function historyFilterHAsync(body) {
     const res = await httpClient.post('/File/HistoryFilterByHospitalId', body);
     if (res.status === 200) {
-        console.log(res.data);
         return res.data;
     } else {
         throw new Error('Without data');
